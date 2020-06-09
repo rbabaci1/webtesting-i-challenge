1b4d8967115e5aa339fcdecdeffb6f7edfafc00c
rabah babaci <4rabah@gmail.com>
2020-06-09 12:43:11 -0700

    move all actual objects to their own test suite

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,41 +1,49 @@
 const { repair, succeed, fail, get } = require("./enhancer.js");
 
-afterEach(() => {
-  item1 = { name: "game-1", enhancement: 10, durability: 35 };
-  item2 = { name: "game-2", enhancement: 20, durability: 35 };
-  item3 = { name: "game-3", enhancement: 15, durability: 35 };
-});
-
-let item1 = { name: "game-1", enhancement: 10, durability: 35 };
-let item2 = { name: "game-2", enhancement: 20, durability: 35 };
-let item3 = { name: "game-3", enhancement: 15, durability: 35 };
-
 describe("enhancement functions", () => {
   describe("repair()", () => {
     it("restores the durability to 100", () => {
+      const actual = { name: "game-1", enhancement: 10, durability: 35 };
       const expected = { name: "game-1", enhancement: 10, durability: 100 };
 
-      expect(repair(item1)).toMatchObject(expected);
+      expect(repair(actual)).toMatchObject(expected);
     });
   });
 
   describe("succeed()", () => {
     it("updates enhancement the item on success", () => {
+      const actual1 = { name: "game-1", enhancement: 10, durability: 35 };
+      const actual2 = { name: "game-2", enhancement: 20, durability: 35 };
+
       const expected1 = { name: "game-1", enhancement: 11, durability: 35 };
       const expected2 = { name: "game-2", enhancement: 20, durability: 35 };
 
-      expect(succeed(item1)).toMatchObject(expected1);
-      expect(succeed(item2)).toMatchObject(expected2);
+      expect(succeed(actual1)).toMatchObject(expected1);
+      expect(succeed(actual2)).toMatchObject(expected2);
     });
   });
 
   describe("updates durability and enhancement on failure", () => {
+    const actual1 = { name: "game-1", enhancement: 10, durability: 35 };
+    const actual2 = { name: "game-2", enhancement: 20, durability: 35 };
+    const actual3 = { name: "game-3", enhancement: 15, durability: 35 };
+
     const expected1 = { name: "game-1", enhancement: 10, durability: 30 };
     const expected2 = { name: "game-2", enhancement: 19, durability: 35 };
     const expected3 = { name: "game-3", enhancement: 15, durability: 25 };
 
-    expect(fail(item1)).toMatchObject(expected1);
-    expect(fail(item2)).toMatchObject(expected2);
-    expect(fail(item3)).toMatchObject(expected3);
+    expect(fail(actual1)).toMatchObject(expected1);
+    expect(fail(actual2)).toMatchObject(expected2);
+    expect(fail(actual3)).toMatchObject(expected3);
+  });
+
+  describe("updates name depending on enhancement level", () => {
+    const actual1 = { name: "game-1", enhancement: 0, durability: 35 };
+    const actual2 = { name: "game-1", enhancement: 10, durability: 35 };
+
+    const expected = { name: "[+10] game-1", enhancement: 10, durability: 35 };
+
+    expect(get(actual1)).toMatchObject(actual1);
+    expect(get(actual2)).toMatchObject(expected);
   });
 });
