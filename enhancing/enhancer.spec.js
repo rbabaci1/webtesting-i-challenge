761e12593ef8786258c1eff0162e6e0429ec3701
rabah babaci <4rabah@gmail.com>
2020-06-09 11:20:17 -0700

    write the repair() test unit

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,14 @@
-const enhancer = require("./enhancer.js");
+const { repair } = require("./enhancer.js");
+
+const item = { name: "game-1", enhancement: 10, durability: 35 };
+
 // test away!
+describe("enhancement functions", () => {
+  describe("repair()", () => {
+    it("restores the durability to 100", () => {
+      const expected = { name: "game-1", enhancement: 10, durability: 100 };
+
+      expect(repair(item)).toMatchObject(expected);
+    });
+  });
+});
