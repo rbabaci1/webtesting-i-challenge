2737931474676f01262ef9a5456a3b46e7499c6a
rabah babaci <4rabah@gmail.com>
2020-06-10 16:15:28 -0700

    create a seperate file for the mockData

---
+++ mockData.js
@@ -0,0 +1,6 @@
+module.exports = [
+  { name: "game-1", enhancement: 10, durability: 35 },
+  { name: "game-2", enhancement: 20, durability: 35 },
+  { name: "game-3", enhancement: 15, durability: 35 },
+  { name: "game-4", enhancement: 0, durability: 35 },
+];
