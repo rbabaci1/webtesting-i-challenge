ce22495b724c9069459743cb00840c88c8709067
rabah babaci <4rabah@gmail.com>
2020-06-10 16:32:26 -0700

    refactor the tests mockData to DRY the code

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,23 +1,18 @@
 const { repair, succeed, fail, get } = require("./enhancer.js");
+let mockData = require("../mockData");
+
+const initialData = JSON.parse(JSON.stringify(mockData));
 
 afterEach(() => {
-  actual1 = { name: "game-1", enhancement: 10, durability: 35 };
-  actual2 = { name: "game-2", enhancement: 20, durability: 35 };
-  actual3 = { name: "game-3", enhancement: 15, durability: 35 };
-  actual4 = { name: "game-4", enhancement: 0, durability: 35 };
+  mockData = JSON.parse(JSON.stringify(initialData));
 });
-
-let actual1 = { name: "game-1", enhancement: 10, durability: 35 };
-let actual2 = { name: "game-2", enhancement: 20, durability: 35 };
-let actual3 = { name: "game-3", enhancement: 15, durability: 35 };
-let actual4 = { name: "game-4", enhancement: 0, durability: 35 };
 
 describe("enhancement functions", () => {
   describe("repair()", () => {
     it("restores the durability to 100", () => {
       const expected = { name: "game-1", enhancement: 10, durability: 100 };
 
-      expect(repair(actual1)).toMatchObject(expected);
+      expect(repair(mockData[0])).toMatchObject(expected);
     });
   });
 
@@ -26,8 +21,8 @@ describe("enhancement functions", () => {
       const expected1 = { name: "game-1", enhancement: 11, durability: 35 };
       const expected2 = { name: "game-2", enhancement: 20, durability: 35 };
 
-      expect(succeed(actual1)).toMatchObject(expected1);
-      expect(succeed(actual2)).toMatchObject(expected2);
+      expect(succeed(mockData[0])).toMatchObject(expected1);
+      expect(succeed(mockData[1])).toMatchObject(expected2);
     });
   });
 
@@ -37,9 +32,9 @@ describe("enhancement functions", () => {
       const expected2 = { name: "game-2", enhancement: 19, durability: 35 };
       const expected3 = { name: "game-3", enhancement: 15, durability: 25 };
 
-      expect(fail(actual1)).toMatchObject(expected1);
-      expect(fail(actual2)).toMatchObject(expected2);
-      expect(fail(actual3)).toMatchObject(expected3);
+      expect(fail(mockData[0])).toMatchObject(expected1);
+      expect(fail(mockData[1])).toMatchObject(expected2);
+      expect(fail(mockData[2])).toMatchObject(expected3);
     });
   });
 
@@ -51,8 +46,8 @@ describe("enhancement functions", () => {
         durability: 35,
       };
 
-      expect(get(actual4)).toMatchObject(actual4);
-      expect(get(actual1)).toMatchObject(expected);
+      expect(get(mockData[3])).toMatchObject(mockData[3]);
+      expect(get(mockData[0])).toMatchObject(expected);
     });
   });
 });
